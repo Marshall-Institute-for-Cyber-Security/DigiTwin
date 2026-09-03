2042b67886ef94751f91b7d1c12cafc324b5ecff
NoahQ <noah@noah.com>
2026-09-03 14:59:07 -0400

    re-export instructions and ExecutiveMode from the package root

--- src/digitwin/__init__.py
+++ src/digitwin/__init__.py
@@ -1,17 +1,23 @@
 """DigiTwin: a small soft-PLC engine and simulation harness."""
 
-from digitwin.executive import Executive
+from digitwin.executive import Executive, ExecutiveMode
+from digitwin.instructions import CTU, ONS, TOF, TON
 from digitwin.io import InProcessTransport, IOBus, IOTransport, IOValue, TransportError
 from digitwin.plant import AnalogSensor, CompositePlant, DiscreteSensor, PlantModel, Tank
 from digitwin.plc import PLC, Program, Tag, TagType, TagValue
 from digitwin.programs import StartStopTankProgram
 
 __all__ = [
+    "CTU",
+    "ONS",
     "PLC",
+    "TOF",
+    "TON",
     "AnalogSensor",
     "CompositePlant",
     "DiscreteSensor",
     "Executive",
+    "ExecutiveMode",
     "IOBus",
     "IOTransport",
     "IOValue",
